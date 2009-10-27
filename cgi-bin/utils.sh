e5b20e8efb6c1e4f19afd2e7c0e2620a9798b524
Charles Karney <karney@users.sourceforge.net>
2009-10-27 21:34:37 +0000

    Translate other (windows?) codes for degree minute second symbols into d ' "

--- cgi-bin/utils.sh
+++ cgi-bin/utils.sh
@@ -3,8 +3,10 @@ lookupkey () {
     QUERY="$1"
     KEY="$2"
     # Convert degree (&B0) minute (&#8242;) second (&#8243;) symbols into d ' ".
+    # and the windows variants(?) %81%8B, %81%8C, %81%8D
     echo "$QUERY" | tr '&' '\n' | grep "^$KEY=" | tail -1 | cut -f2- -d= |
     sed -e 's/%B0/d/g' -e 's/%26%238242%3B/%27/g' -e 's/%26%238243%3B/%22/g' \
+	-e 's/%81%8B/d/g' -e 's/%81%8C/%27/g' -e 's/%81%8D/%22/g' \
 	-e 's/\\/%5C/g' -e 's/%\([0-9a-fA-f][0-9a-fA-F]\)/\\x\1/g' -e s/%/%%/g |
     xargs -d '\n' printf | tr -s '+' ' '
 }
