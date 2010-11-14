f105596ea1f6fb74e67c58b729a878737bb7c477
Charles Karney <karney@users.sourceforge.net>
2010-11-14 23:05:37 +0000

    More cleanup in LambertConformalConic. Add epi() and edegree().

--- GeographicLib/tests/ConicTest.cpp
+++ GeographicLib/tests/ConicTest.cpp
@@ -8,10 +8,8 @@
  **********************************************************************/
 
 #include "GeographicLib/LambertConformalConic.hpp"
-#include "GeographicLib/PolarStereographic.hpp"
-#include "GeographicLib/TransverseMercator.hpp"
-#include "GeographicLib/TransverseMercatorExact.hpp"
 #include "GeographicLib/Constants.hpp"
+#include "GeographicLib/DMS.hpp"
 #include <string>
 #include <limits>
 #include <iostream>
@@ -55,10 +53,22 @@ int main(int argc, char* argv[]) {
   using namespace GeographicLib;
   using namespace std;
   typedef Math::real real;
-  //  Math::extended degree = atan2(Math::extended(1), Math::extended(1))/45;
+  if (false) {
+    for (int i = -12; i <= 0; ++i) {
+      Math::extended
+        colat = std::pow(Math::extended(10), i),
+        lat = 90 - colat,
+        coslat1 = cos(lat*Math::edegree()),
+        coslat2 = sin(colat*Math::edegree());
+      std::cout << i << " " << coslat1 << " " << coslat2 << " " <<  (coslat1-coslat2)/coslat2 << "\n";
+      std::cout << i << " " << asin(coslat1)/Math::edegree() - colat << "\n";
+    }
+    return 0;
+  }
   bool lambert = true;
   bool albert = false;
   bool checkstdlats = false;
+  real a = Constants::WGS84_a(), r = Constants::WGS84_r();
   for (int m = 1; m < argc; ++m) {
     std::string arg(argv[m]);
     if (arg == "-l") {
@@ -66,19 +76,56 @@ int main(int argc, char* argv[]) {
       albert = false;
     } else if (arg == "-s") {
       checkstdlats = true;
+    } else if (arg == "-e") {
+      if (m + 2 >= argc) return usage(1);
+      try {
+        a = DMS::Decode(std::string(argv[m + 1]));
+        r = DMS::Decode(std::string(argv[m + 2]));
+      }
+      catch (const std::exception& e) {
+        std::cerr << "Error decoding arguments of -e: " << e.what() << "\n";
+        return 1;
+      }
+      m += 2;
     } else
       return usage(arg != "-h");
   }
 
-  real a = Constants::WGS84_a(), r = Constants::WGS84_r();
   try {
+    std::cout << std::setprecision(14);
+    Math::extended quant = 1e12L;
     while (true) {
       Math::extended lat1, lat2, lat0, k0;
       if (!(cin >> lat1 >> lat2 >> lat0 >> k0))
-	break;
-      const LambertConformalConic lam(a, r, real(lat1), real(lat2), real(1));
+        break;
+      int
+        sign1 = lat1 < 0 ? -1 : 1,
+        sign2 = lat2 < 0 ? -1 : 1;
+      lat1 = floor(sign1 * lat1 * quant + 0.5L);
+      lat2 = floor(sign2 * lat2 * quant + 0.5L);
+      Math::extended
+        colat1 = (90 * quant - lat1) / quant,
+        colat2 = (90 * quant - lat2) / quant;
+      lat1 /= quant;
+      lat2 /= quant;
+      Math::extended
+        sinlat1 = sign1 * (lat1 < 45 ? sin(lat1 * Math::edegree())
+                           : cos(colat1 * Math::edegree())),
+        sinlat2 = sign2 * (lat2 < 45 ? sin(lat2 * Math::edegree())
+                           : cos(colat2 * Math::edegree())),
+        coslat1 = (lat1 < 45 ? cos(lat1 * Math::edegree())
+                   : sin(colat1 * Math::edegree())),
+        coslat2 = (lat2 < 45 ? cos(lat2 * Math::edegree())
+                   : sin(colat2 * Math::edegree()));
+      lat1 *= sign1;
+      lat2 *= sign2;
+      const LambertConformalConic lam(a, r, /* real(lat1), real(lat2), */
+                                      real(sinlat1), real(coslat1),
+                                      real(sinlat2), real(coslat2),
+                                      real(1));
       Math::extended lat0a = lam.OriginLatitude(), k0a = lam.CentralScale();
-      cout << lat1 << " " << lat2 << " " << lat0 << " " << lat0a << " " << lat0a - lat0 << " " << k0a - k0 << "\n";
+      if (abs(lat0a-lat0) > 0.4e-13L || abs(k0a - k0) > 7e-15L * k0 )
+        cout << lat1 << " " << lat2 << " " << lat0 << " " << lat0a << " " << lat0a - lat0 << " " << (k0a - k0)/k0 << "\n";
       /*
       const LambertConformalConic lamb(a, r, real(sin(lat1*degree)), real(cos(lat1*degree)), real(sin(lat2*degree)), real(cos(lat2*degree)), real(1));
       lat0a = lamb.OriginLatitude(); k0a = lamb.CentralScale();
