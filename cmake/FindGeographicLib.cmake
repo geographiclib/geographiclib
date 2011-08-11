dcb3cce032caf22b46e28a41553285fabdb34c3b
Charles Karney <karney@users.sourceforge.net>
2011-08-11 11:15:46 -0400

    Get capitalization of cmake variables consistent.

--- cmake/FindGeographicLib.cmake
+++ cmake/FindGeographicLib.cmake
@@ -4,31 +4,29 @@
 #
 # Set
 #  GEOGRAPHICLIB_FOUND = TRUE
-#  GEOGRAPHICLIB_INCLUDE_DIRS = /usr/local/include
-#  GEOGRAPHICLIB_LIBRARIES = /usr/local/lib/libGeographic.so
-#  GEOGRAPHICLIB_LIBRARY_DIRS = /usr/local/lib
+#  GeographicLib_INCLUDE_DIRS = /usr/local/include
+#  GeographicLib_LIBRARIES = /usr/local/lib/libGeographic.so
+#  GeographicLib_LIBRARY_DIRS = /usr/local/lib
 
-find_library (GEOGRAPHICLIB_LIBRARIES Geographic
+find_library (GeographicLib_LIBRARIES Geographic
   PATHS "${CMAKE_INSTALL_PREFIX}/../GeographicLib/lib")
 
-if (GEOGRAPHICLIB_LIBRARIES)
-  get_filename_component (GEOGRAPHICLIB_LIBRARY_DIRS
-    "${GEOGRAPHICLIB_LIBRARIES}" PATH)
+if (GeographicLib_LIBRARIES)
+  get_filename_component (GeographicLib_LIBRARY_DIRS
+    "${GeographicLib_LIBRARIES}" PATH)
   get_filename_component (GEOGRAPHICLIB_ROOT_DIR
-    "${GEOGRAPHICLIB_LIBRARY_DIRS}" PATH)
-  set (GEOGRAPHICLIB_INCLUDE_DIRS "${GEOGRAPHICLIB_ROOT_DIR}/include")
+    "${GeographicLib_LIBRARY_DIRS}" PATH)
+  set (GeographicLib_INCLUDE_DIRS "${GEOGRAPHICLIB_ROOT_DIR}/include")
   unset (GEOGRAPHICLIB_ROOT_DIR)
-  if (NOT EXISTS "${GEOGRAPHICLIB_INCLUDE_DIRS}/GeographicLib/Config.h")
-    unset (GEOGRAPHICLIB_INCLUDE_DIRS)
-    unset (GEOGRAPHICLIB_LIBRARIES)
-    unset (GEOGRAPHICLIB_LIBRARY_DIRS)
-  endif (NOT EXISTS "${GEOGRAPHICLIB_INCLUDE_DIRS}/GeographicLib/Config.h")
-endif (GEOGRAPHICLIB_LIBRARIES)
+  if (NOT EXISTS "${GeographicLib_INCLUDE_DIRS}/GeographicLib/Config.h")
+    unset (GeographicLib_INCLUDE_DIRS)
+    unset (GeographicLib_LIBRARIES)
+    unset (GeographicLib_LIBRARY_DIRS)
+  endif (NOT EXISTS "${GeographicLib_INCLUDE_DIRS}/GeographicLib/Config.h")
+endif (GeographicLib_LIBRARIES)
 
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args (GeographicLib DEFAULT_MSG
-  GEOGRAPHICLIB_INCLUDE_DIRS
-  GEOGRAPHICLIB_LIBRARIES
-  GEOGRAPHICLIB_LIBRARY_DIRS)
-mark_as_advanced (GEOGRAPHICLIB_INCLUDE_DIRS
-  GEOGRAPHICLIB_LIBRARIES GEOGRAPHICLIB_LIBRARY_DIRS)
+  GeographicLib_LIBRARY_DIRS GeographicLib_LIBRARIES GeographicLib_INCLUDE_DIRS)
+mark_as_advanced (GeographicLib_LIBRARY_DIRS GeographicLib_LIBRARIES
+  GeographicLib_INCLUDE_DIRS)
