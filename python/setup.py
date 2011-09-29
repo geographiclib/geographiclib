355d6d19ea2fc6ee8e414b75c57592f3e241e3b6
Charles Karney <karney@users.sourceforge.net>
2011-09-29 16:36:50 -0400

    documentation, installation, distribution for python/setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,24 +1,34 @@
 # setup.py, config file for distutils
 #
-# To build a windows installer for this package, execute
+# To install this package, execute
 #
-#   python setup.py bdist_wininst
+#   python setup.py install
 #
-# in this directory.  The resulting installer will be in the dist
-# subdirectory.
+# in this directory.  The initial version of this file was provided by
+# Andrew MacIntyre <Andrew.MacIntyre@acma.gov.au>.
 #
 # $Id$
 
 from distutils.core import setup
 
 setup(name="geographiclib",
-      version="0.13",
+      version="1.14",
       description=
         "A translation of the GeographicLib::Geodesic class to Python",
       author="Charles Karney",
       author_email="charles@karney.com",
-      url="http://geographiclib.sourceforge.net",
+      url="http://geographiclib.sourceforge.net/",
       packages=["geographiclib"],
       data_files=[],
       license="MIT",
+      keywords="gis geographical earth distance geodesic",
+      classifiers=["Development Status :: 5 - Production/Stable",
+                   "Intended Audience :: Developers",
+                   "Intended Audience :: Science/Research",
+                   "License :: OSI Approved :: MIT License",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Scientific/Engineering :: GIS",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                   ],
       )
