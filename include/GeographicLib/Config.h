d521f3d7c39a49ed4b4b306a7a47251b0e343689
Charles Karney <karney@users.sourceforge.net>
2011-11-07 17:34:11 -0500

    Put WORDS_BIGENDIAN into skeleton Config.h.

--- include/GeographicLib/Config.h
+++ include/GeographicLib/Config.h
@@ -2,3 +2,9 @@
 // This will be overwritten by ./configure
 
 #define GEOGRAPHICLIB_VERSION_STRING "Unconfigured"
+
+// Undefine HAVE_LONG_DOUBLE if this type is unknown to the compiler
+#define HAVE_LONG_DOUBLE 1
+
+// Define WORDS_BIGENDIAN to be 1 if your machine is big endian
+/* #undef WORDS_BIGENDIAN */
