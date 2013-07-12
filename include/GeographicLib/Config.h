1b1e80d4975fdbb68d6391ec0ffca64a322fad96
Charles Karney <charles.karney@sri.com>
2013-07-12 09:34:38 -0400

    Version 1.32 (2013-07-12)

--- include/GeographicLib/Config.h
+++ include/GeographicLib/Config.h
@@ -1,8 +1,8 @@
 // This will be overwritten by ./configure
 
-#define GEOGRAPHICLIB_VERSION_STRING "1.31"
+#define GEOGRAPHICLIB_VERSION_STRING "1.32"
 #define GEOGRAPHICLIB_VERSION_MAJOR 1
-#define GEOGRAPHICLIB_VERSION_MINOR 31
+#define GEOGRAPHICLIB_VERSION_MINOR 32
 #define GEOGRAPHICLIB_VERSION_PATCH 0
 
 // Undefine HAVE_LONG_DOUBLE if this type is unknown to the compiler
