ed1b0332e2a8fa2a1dcdd2d796f433ab685d55f7
Charles Karney <karney@users.sourceforge.net>
2013-12-03 11:14:23 -0500

    Fix cmake tests.

--- tests/cmake/build-geographiclib.sh
+++ tests/cmake/build-geographiclib.sh
@@ -1,22 +1,29 @@
 #! /bin/sh -e
 
-for v in 0 1; do
-    (
-	b=geogbuild$v
-	inst=geoginst$v/GeographicLib-1.34
-	rm -rf /tmp/$b /tmp/$inst
-	mkdir /tmp/$b
-	cd /tmp/$b
-	case $v in
-	    0 ) cmake=/u/temp/cmake-2.8.0/bin/cmake;;
-	    1 ) cmake=cmake;;
-	esac
-	$cmake -G "Visual Studio 10" -D GEOGRAPHICLIB_LIB_TYPE=BOTH -D CMAKE_INSTALL_PREFIX=c:/tmp/$inst -D BUILD_NETGEOGRAPHICLIB=ON u:/geographiclib
-	$cmake --build . --config Debug --target ALL_BUILD
-	$cmake --build . --config Debug --target RUN_TESTS
-	$cmake --build . --config Debug --target INSTALL
-	$cmake --build . --config Release --target ALL_BUILD
-	$cmake --build . --config Release --target RUN_TESTS
-	$cmake --build . --config Release --target INSTALL
-    )
+for v in 1 0; do
+    for t in "" a; do
+	(
+	    b=geogbuild$v$t
+	    inst=geoginst$v$t/GeographicLib-1.34
+	    rm -rf /tmp/$b /tmp/$inst
+	    mkdir /tmp/$b
+	    cd /tmp/$b
+	    case $v in
+		0 ) cmake=/u/temp/cmake-2.8.0/bin/cmake;;
+		1 ) cmake=cmake;;
+	    esac
+	    if test "$t"; then
+		lib=STATIC
+	    else
+		lib=BOTH
+	    fi
+	    $cmake -G "Visual Studio 10" -D GEOGRAPHICLIB_LIB_TYPE=$lib -D CMAKE_INSTALL_PREFIX=c:/tmp/$inst -D BUILD_NETGEOGRAPHICLIB=ON u:/geographiclib
+	    $cmake --build . --config Debug --target ALL_BUILD
+	    $cmake --build . --config Debug --target RUN_TESTS
+	    $cmake --build . --config Debug --target INSTALL
+	    $cmake --build . --config Release --target ALL_BUILD
+	    $cmake --build . --config Release --target RUN_TESTS
+	    $cmake --build . --config Release --target INSTALL
+	)
+    done
 done
