12783fc29dd0f5486e1e74488f16e3b11b6bcf1a
Charles Karney <karney@users.sourceforge.net>
2014-12-30 13:39:55 -0500

    Remove superceded material on documentation of Jacobi conformal proj.

--- examples/JacobiConformal.hpp
+++ examples/JacobiConformal.hpp
@@ -1,6 +1,6 @@
 /**
  * \file JacobiConformal.hpp
- * \brief A class for Jacobi's conformal projection of a triaxial ellipsoid.
+ * \brief Jacobi's conformal projection of a triaxial ellipsoid.
  *
  * Copyright (c) Charles Karney (2014) <charles@karney.com> and licensed
  * under the MIT/X11 License.  For more information, see
@@ -13,15 +13,15 @@ namespace GeographicLib {
   /**
    * \brief Jacobi's conformal projection
    *
-   * This is a conformal projection of the ellipsoid to a plane in which the
-   * grid lines are straight; see Jacobi, Vorlesungen ueber Dynamik, Sect. 28.
-   * The constructor takes the semi-axes of the ellipsoid (which must be in
-   * order).  Member functions map the ellipsoidal coordinates &omega; and
-   * &beta; separately to \e x and \e y.  Jacobi's coordinates have been
-   * multiplied by
+   * This is a conformal projection of the ellipsoid to a plane in which
+   * the grid lines are straight; see Jacobi, Vorlesungen ueber Dynamik,
+   * Sect. 28.  The constructor takes the semi-axes of the ellipsoid (which
+   * must be in order).  Member functions map the ellipsoidal coordinates
+   * &omega; and &beta; separately to \e x and \e y.  Jacobi's coordinates
+   * have been multiplied by
    * (<i>a</i><sup>2</sub>&minus;<i>c</i><sup>2</sub>)<sup>1/2</sup> /
-   * (2<i>b</i>) so that the customary results are returned in the cases of a
-   * sphere or an ellipsoid of revolution.
+   * (2<i>b</i>) so that the customary results are returned in the cases of
+   * a sphere or an ellipsoid of revolution.
    **********************************************************************/
   class JacobiConformal {
     Math::real _a, _b, _c, _ab2, _bc2, _ac2;
@@ -36,9 +36,9 @@ namespace GeographicLib {
      * @param[in] b
      * @param[in] c
      *
-     * The semi-axes must satisfy \e a &ge; \e b &ge; \e c > 0 and \e a > \e c.
-     * This form of the constructor cannot be used to specify a sphere (use the
-     * next constructor).
+     * The semi-axes must satisfy \e a &ge; \e b &ge; \e c > 0 and \e a >
+     * \e c.  This form of the constructor cannot be used to specify a
+     * sphere (use the next constructor).
      **********************************************************************/
     JacobiConformal(Math::real a, Math::real b, Math::real c)
       : _a(a), _b(b), _c(c)
@@ -63,10 +63,10 @@ namespace GeographicLib {
      * @param[in] ab the relative magnitude of \e a &minus \e b.
      * @param[in] bc the relative magnitude of \e b &minus \e c.
      *
-     * This form can be used to specify a sphere.  The semi-axes must satisfy
-     * \e a &ge \e b &ge c > 0.  The ratio \e ab : \e bc must equal
-     * (<i>a</i>&minus;<i>b</i>) : (<i>b</i>&minus;<i>c</i>) with \e ab &ge; 0,
-     * \e bc &be; 0, and \e ab + \e bc > 0.
+     * This form can be used to specify a sphere.  The semi-axes must
+     * satisfy \e a &ge \e b &ge c > 0.  The ratio \e ab : \e bc must equal
+     * (<i>a</i>&minus;<i>b</i>) : (<i>b</i>&minus;<i>c</i>) with \e ab
+     * &ge; 0, \e bc &be; 0, and \e ab + \e bc > 0.
      **********************************************************************/
     JacobiConformal(Math::real a, Math::real b, Math::real c,
                     Math::real ab, Math::real bc)
@@ -82,7 +82,8 @@ namespace GeographicLib {
             _ab2 / _ac2 * Math::sq(_c / _b), Math::sq(_c / _b))
     {
       using std::sqrt;
-      if (!(a >= b && b >= c && c > 0 && ab >= 0 &&  bc >= 0 && ab + ab > 0))
+      if (!(a >= b && b >= c && c > 0 &&
+            ab >= 0 &&  bc >= 0 && ab + ab > 0))
         throw GeographicErr("axes are not in order");
     }
     /**
@@ -98,7 +99,8 @@ namespace GeographicLib {
      **********************************************************************/
     Math::real x(Math::real somg, Math::real comg) const {
       Math::real somg1 = _b * somg, comg1 = _a * comg; norm(somg1, comg1);
-      return Math::sq(_a / _b) * _ex.Pi(somg1, comg1, _ex.Delta(somg1, comg1));
+      return Math::sq(_a / _b)
+        * _ex.Pi(somg1, comg1, _ex.Delta(somg1, comg1));
     }
     /**
      * The \e x projection
@@ -129,7 +131,8 @@ namespace GeographicLib {
      **********************************************************************/
     Math::real y(Math::real sbet, Math::real cbet) const {
       Math::real sbet1 = _b * sbet, cbet1 = _c * cbet; norm(sbet1, cbet1);
-      return Math::sq(_c / _b) * _ey.Pi(sbet1, cbet1, _ey.Delta(sbet1, cbet1));
+      return Math::sq(_c / _b)
+        * _ey.Pi(sbet1, cbet1, _ey.Delta(sbet1, cbet1));
     }
     /**
      * The \e y projection
