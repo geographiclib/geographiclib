25e2111d713170d4fb247e1966e4173d50dd4ed8
Charles Karney <karney@users.sourceforge.net>
2014-12-30 12:37:02 -0500

    Document improved Jacobi conformal, and clean up JacobiConformal.hpp.

--- examples/JacobiConformal.cpp
+++ examples/JacobiConformal.cpp
@@ -9,25 +9,15 @@ using namespace GeographicLib;
 int main() {
   Utility::set_digits();
   Math::real a = 6378137+35, b = 6378137-35, c = 6356752;
-  JacobiConformal jc(a, b, c);
+  JacobiConformal jc(a, b, c, a-b, b-c);
   cout  << fixed << setprecision(1)
         << "Ellipsoid parameters: a = "
         << a << ", b = " << b << ", c = " << c << "\n"
         << setprecision(10)
-        << "Quadrants: x = " << jc.x() << ", y = " << jc.y() << "\n"
-        << "Quadrants: x = " << jc.x_0() << ", y = " << jc.y_0() << "\n"
-        << "Quadrants: x = " << jc.x_b() << ", y = " << jc.y_b() << "\n"
-        << "Quadrants: x = " << jc.x_bx() << ", y = " << jc.y_bx() << "\n"
-        << "Quadrants: x = " << jc.x_c() << ", y = " << jc.y_c() << "\n"
-        << "Quadrants: x = " << jc.x_bx() << ", y = " << jc.y_bx() << "\n"
-        << "Quadrants: x = " << jc.x_cx() << ", y = " << jc.y_cx() << "\n";
-  Math::real f = 1/Math::degree();
-  cout << "Scaled coordinates (angle x y):\n";
+        << "Quadrants: x = " << jc.x() << ", y = " << jc.y() << "\n";
+  cout << "Coordinates (angle x y) in degrees:\n";
   for (int i = 0; i <= 90; i += 5) {
     Math::real omg = i, bet = i;
-    cout << i << " " << (jc.x()-jc.x(90-omg))*f << " " << jc.y(bet)*f << "\n";
-    cout << i << " " << jc.x_0(omg)*f << " " << jc.y_0(bet)*f << "\n";
-    cout << i << " " << jc.x_b(omg)*f << " " << jc.y_b(bet)*f << "\n";
-    cout << i << " " << jc.x_c(omg)*f << " " << jc.y_c(bet)*f << "\n";
+    cout << i << " " << jc.x(omg) << " " << jc.y(bet) << "\n";
   }
 }
--- examples/JacobiConformal.hpp
+++ examples/JacobiConformal.hpp
@@ -10,22 +10,22 @@
 #include <GeographicLib/EllipticFunction.hpp>
 
 namespace GeographicLib {
-
   /**
    * \brief Jacobi's conformal projection
    *
    * This is a conformal projection of the ellipsoid to a plane in which the
    * grid lines are straight; see Jacobi, Vorlesungen ueber Dynamik, Sect. 28.
-   * The constructor takes the semi-axes of the ellipsoid (which must be
-   * scalene).  Member functions map the ellipsoidal coordinates &omega; and
-   * &beta; separately to \e x and \e y.
+   * The constructor takes the semi-axes of the ellipsoid (which must be in
+   * order).  Member functions map the ellipsoidal coordinates &omega; and
+   * &beta; separately to \e x and \e y.  Jacobi's coordinates have been
+   * multiplied by
+   * (<i>a</i><sup>2</sub>&minus;<i>c</i><sup>2</sub>)<sup>1/2</sup> /
+   * (2<i>b</i>) so that the customary results are returned in the cases of a
+   * sphere or an ellipsoid of revolution.
    **********************************************************************/
   class JacobiConformal {
-    Math::real _a, _b, _c, _ab2, _bc2, _ac2, _ab, _bc, _ac;
+    Math::real _a, _b, _c, _ab2, _bc2, _ac2;
     EllipticFunction _ex, _ey;
-    EllipticFunction _exa, _eya;
-    EllipticFunction _exb, _eyb;
-    EllipticFunction _exc, _eyc;
     static void norm(Math::real& x, Math::real& y)
     { Math::real z = Math::hypot(x, y); x /= z; y /= z; }
   public:
@@ -36,45 +36,59 @@ namespace GeographicLib {
      * @param[in] b
      * @param[in] c
      *
-     * The semi-axes must satisfy \e a > \e b > \e c > 0.
+     * The semi-axes must satisfy \e a &ge; \e b &ge; \e c > 0 and \e a > \e c.
+     * This form of the constructor cannot be used to specify a sphere (use the
+     * next constructor).
      **********************************************************************/
     JacobiConformal(Math::real a, Math::real b, Math::real c)
       : _a(a), _b(b), _c(c)
       , _ab2((_a - _b) * (_a + _b))
       , _bc2((_b - _c) * (_b + _c))
       , _ac2((_a - _c) * (_a + _c))
-      , _ex (+_ab2 / _ac2 * Math::sq(_c / _b), -_ab2 / Math::sq(_b),
-             +_bc2 / _ac2 * Math::sq(_a / _b), Math::sq(_a / _b))
-      , _ey (+_bc2 / _ac2 * Math::sq(_a / _b), +_bc2 / Math::sq(_b),
-             +_ab2 / _ac2 * Math::sq(_c / _b), Math::sq(_c / _b))
-      , _exa(+_ab2 / _ac2 * Math::sq(_c / _b), -_ab2 / Math::sq(_b),
-             +_bc2 / _ac2 * Math::sq(_a / _b), +Math::sq(_a / _b))
-      , _eya(-_bc2 / _ab2 * Math::sq(_a / _c), -_bc2 / Math::sq(_c),
-             +_ac2 / _ab2 * Math::sq(_b / _c), +Math::sq(_b / _c))
-      , _exb(-_ab2 / _bc2 * Math::sq(_c / _a), -_ab2 / _bc2,
-             +_ac2 / _bc2 * Math::sq(_b / _a), +_ac2 / _bc2)
-      , _eyb(-_bc2 / _ab2 * Math::sq(_a / _c), -_bc2 / _ab2,
-             +_ac2 / _ab2 * Math::sq(_b / _c), +_ac2 / _ab2)
-      , _exc(-_ab2 / _bc2 * Math::sq(_c / _a), +_ab2 / Math::sq(_a),
-             +_ac2 / _bc2 * Math::sq(_b / _a), +Math::sq(_b / _a))
-      , _eyc(-_bc2 / _ab2 * Math::sq(_a / _c), -_bc2 / Math::sq(_c),
-             +_ac2 / _ab2 * Math::sq(_b / _c), +Math::sq(_b / _c))
+      , _ex(_ab2 / _ac2 * Math::sq(_c / _b), -_ab2 / Math::sq(_b),
+            _bc2 / _ac2 * Math::sq(_a / _b), Math::sq(_a / _b))
+      , _ey(_bc2 / _ac2 * Math::sq(_a / _b), +_bc2 / Math::sq(_b),
+            _ab2 / _ac2 * Math::sq(_c / _b), Math::sq(_c / _b))
     {
       using std::sqrt;
-      if (!(a > b && b > c && c > 0))
+      if (!(a >= b && b >= c && c > 0 && a > c))
         throw GeographicErr("axes are not in order");
-      _ab = sqrt(_ab2);
-      _bc = sqrt(_bc2);
-      _ac = sqrt(_ac2);
-      // tan(nu) = _bc/_ab; sin(nu) = _bc/_ac; cos(nu) = _ab/_ac
-      // cot(nu) = _ab/_bc; csc(nu) = _ac/_bc; sec(nu) = _ac/_ab
+    }
+    /**
+     * Constructor for a triaxial ellipsoid with semi-axes
+     *
+     * @param[in] a
+     * @param[in] b
+     * @param[in] c
+     * @param[in] ab the relative magnitude of \e a &minus \e b.
+     * @param[in] bc the relative magnitude of \e b &minus \e c.
+     *
+     * This form can be used to specify a sphere.  The semi-axes must satisfy
+     * \e a &ge \e b &ge c > 0.  The ratio \e ab : \e bc must equal
+     * (<i>a</i>&minus;<i>b</i>) : (<i>b</i>&minus;<i>c</i>) with \e ab &ge; 0,
+     * \e bc &be; 0, and \e ab + \e bc > 0.
+     **********************************************************************/
+    JacobiConformal(Math::real a, Math::real b, Math::real c,
+                    Math::real ab, Math::real bc)
+      : _a(a), _b(b), _c(c)
+      , _ab2(ab * (_a + _b))
+      , _bc2(bc * (_b + _c))
+      , _ac2(_ab2 + _bc2)
+      , _ex(_ab2 / _ac2 * Math::sq(_c / _b),
+            -(_a - _b) * (_a + _b) / Math::sq(_b),
+            _bc2 / _ac2 * Math::sq(_a / _b), Math::sq(_a / _b))
+      , _ey(_bc2 / _ac2 * Math::sq(_a / _b),
+            +(_b - _c) * (_b + _c) / Math::sq(_b),
+            _ab2 / _ac2 * Math::sq(_c / _b), Math::sq(_c / _b))
+    {
+      using std::sqrt;
+      if (!(a >= b && b >= c && c > 0 && ab >= 0 &&  bc >= 0 && ab + ab > 0))
+        throw GeographicErr("axes are not in order");
     }
     /**
      * @return the quadrant length in the \e x direction
      **********************************************************************/
-    Math::real x() const {
-      return (_ac/(2*_b)) * 2 * Math::sq(_a) / (_b * _ac) * _exa.Pi();
-    }
+    Math::real x() const { return Math::sq(_a / _b) * _ex.Pi(); }
     /**
      * The \e x projection
      *
@@ -83,20 +97,14 @@ namespace GeographicLib {
      * @return \e x
      **********************************************************************/
     Math::real x(Math::real somg, Math::real comg) const {
-      using std::sqrt; using std::atan;
-      Math::real somg1 = _ac * somg, comg1 = _bc * comg;
-      norm(somg1, comg1);
-      Math::real domg1 = _exa.Delta(somg1, comg1);
-      return (_ac/(2*_b)) * 2 *
-        ( Math::sq(_a) / (_b * _ac) * _exa.Pi(somg1, comg1, domg1) -
-          atan(_ab2 * somg1 * comg1 / sqrt(Math::sq(_a * somg1) * _bc2 +
-                                           Math::sq(_b * comg1) * _ac2)) );
+      Math::real somg1 = _b * somg, comg1 = _a * comg; norm(somg1, comg1);
+      return Math::sq(_a / _b) * _ex.Pi(somg1, comg1, _ex.Delta(somg1, comg1));
     }
     /**
      * The \e x projection
      *
      * @param[in] omg &omega; (in degrees)
-     * @return \e x
+     * @return \e x (in degrees)
      *
      * &omega; must be in (&minus;180&deg;, 180&deg;].
      **********************************************************************/
@@ -106,14 +114,12 @@ namespace GeographicLib {
         a = omg * Math::degree(),
         somg = abs(omg) == 180 ? 0 : sin(a),
         comg = abs(omg) ==  90 ? 0 : cos(a);
-      return x(somg, comg);
+      return x(somg, comg) / Math::degree();
     }
     /**
      * @return the quadrant length in the \e y direction
      **********************************************************************/
-    Math::real y() const {
-      return (_ac/(2*_b)) * 2 * Math::sq(_b) / (_c * _ab) * _eya.Pi();
-    }
+    Math::real y() const { return Math::sq(_c / _b) * _ey.Pi(); }
     /**
      * The \e y projection
      *
@@ -122,20 +128,14 @@ namespace GeographicLib {
      * @return \e y
      **********************************************************************/
     Math::real y(Math::real sbet, Math::real cbet) const {
-      using std::sqrt;
-      Math::real sbet1 = _ab * sbet, cbet1 = _ac * cbet;
-      norm(sbet1, cbet1);
-      Math::real dbet1 = _eya.Delta(sbet1, cbet1);
-      return (_ac/(2*_b)) * 2 *
-        ( Math::sq(_b) / (_c * _ab) * _eya.Pi(sbet1, cbet1, dbet1) -
-          Math::asinh(_bc2 * sbet * cbet / sqrt(Math::sq(_b * sbet) * _ab2 +
-                                                Math::sq(_c * cbet) * _ac2 )) );
+      Math::real sbet1 = _b * sbet, cbet1 = _c * cbet; norm(sbet1, cbet1);
+      return Math::sq(_c / _b) * _ey.Pi(sbet1, cbet1, _ey.Delta(sbet1, cbet1));
     }
     /**
      * The \e y projection
      *
      * @param[in] bet &beta; (in degrees)
-     * @return \e y
+     * @return \e y (in degrees)
      *
      * &beta; must be in (&minus;180&deg;, 180&deg;].
      **********************************************************************/
@@ -145,149 +145,7 @@ namespace GeographicLib {
         a = bet * Math::degree(),
         sbet = abs(bet) == 180 ? 0 : sin(a),
         cbet = abs(bet) ==  90 ? 0 : cos(a);
-      return y(sbet, cbet);
-    }
-
-    Math::real x_0() const {
-      return Math::sq(_a / _b) * _ex.Pi();
-    }
-    Math::real x_0(Math::real somg, Math::real comg) const {
-      Math::real somg1 = _b * somg, comg1 = _a * comg;
-      norm(somg1, comg1);
-      Math::real domg1 = _ex.Delta(somg1, comg1);
-      return Math::sq(_a / _b) * _ex.Pi(somg1, comg1, domg1);
-    }
-    Math::real x_0(Math::real omg) const {
-      using std::abs; using std::sin; using std::cos;
-      Math::real
-        a = omg * Math::degree(),
-        somg = abs(omg) == 180 ? 0 : sin(a),
-        comg = abs(omg) ==  90 ? 0 : cos(a);
-      return x_0(somg, comg);
-    }
-    Math::real y_0() const {
-      return Math::sq(_c / _b) * _ey.Pi();
-    }
-    Math::real y_0(Math::real sbet, Math::real cbet) const {
-      Math::real sbet1 = _b * sbet, cbet1 = _c * cbet;
-      norm(sbet1, cbet1);
-      Math::real dbet1 = _ey.Delta(sbet1, cbet1);
-      return Math::sq(_c / _b) * _ey.Pi(sbet1, cbet1, dbet1);
-    }
-    Math::real y_0(Math::real bet) const {
-      using std::abs; using std::sin; using std::cos;
-      Math::real
-        a = bet * Math::degree(),
-        sbet = abs(bet) == 180 ? 0 : sin(a),
-        cbet = abs(bet) ==  90 ? 0 : cos(a);
-      return y_0(sbet, cbet);
-    }
-
-    Math::real x_b() const {
-      return (_a * _ac) / (_b * _bc) * _exb.G();
-    }
-    Math::real x_b(Math::real somg, Math::real comg) const {
-      using std::sqrt; using std::atan;
-      Math::real somg1 = _bc * somg, comg1 = _ac * comg;
-      norm(somg1, comg1);
-      Math::real domg1 = _exb.Delta(somg1, comg1);
-      return  (_a * _ac) / (_b * _bc) * _exb.G(somg1, comg1, domg1);
-    }
-    Math::real x_b(Math::real omg) const {
-      using std::abs; using std::sin; using std::cos;
-      Math::real
-        a = omg * Math::degree(),
-        somg = abs(omg) == 180 ? 0 : sin(a),
-        comg = abs(omg) ==  90 ? 0 : cos(a);
-      return x_b(somg, comg);
-    }
-    Math::real y_b() const {
-      return  (_c * _ac) / (_b * _ab) * _eyb.G();
-    }
-    Math::real y_b(Math::real sbet, Math::real cbet) const {
-      using std::sqrt;
-      Math::real sbet1 = _ab * sbet, cbet1 = _ac * cbet;
-      norm(sbet1, cbet1);
-      Math::real dbet1 = _eyb.Delta(sbet1, cbet1);
-      return  (_c * _ac) / (_b * _ab) * _eyb.G(sbet1, cbet1, dbet1);
-    }
-    Math::real y_b(Math::real bet) const {
-      using std::abs; using std::sin; using std::cos;
-      Math::real
-        a = bet * Math::degree(),
-        sbet = abs(bet) == 180 ? 0 : sin(a),
-        cbet = abs(bet) ==  90 ? 0 : cos(a);
-      return y_b(sbet, cbet);
-    }
-
-    Math::real x_c() const {
-      return (_b * _ac) / (_a * _bc) * _exc.Pi();
-    }
-    Math::real x_c(Math::real somg, Math::real comg) const {
-      using std::sqrt; using std::atan; using std::asin;
-      Math::real somg1 = _bc * somg, comg1 = _ac * comg;
-      norm(somg1, comg1);
-      Math::real domg1 = _exc.Delta(somg1, comg1);
-      return (_ac/_b) *
-        ( Math::sq(_b) / (_a * _bc) * _exc.Pi(somg1, comg1, domg1) +
-          (0 ?
-           asin(_ab2 * somg * comg / sqrt(Math::sq(_b * somg) * _bc2 +
-                                          Math::sq(_a * comg) * _ac2))
-           :
-           atan(_ab2 * somg1 * comg1 / sqrt(Math::sq(_b * somg1) * _ac2 +
-                                            Math::sq(_a * comg1) * _bc2))) );
-    }
-    Math::real x_c(Math::real omg) const {
-      using std::abs; using std::sin; using std::cos;
-      Math::real
-        a = omg * Math::degree(),
-        somg = abs(omg) == 180 ? 0 : sin(a),
-        comg = abs(omg) ==  90 ? 0 : cos(a);
-      return x_c(somg, comg);
-    }
-    Math::real y_c() const {
-      return  (_b * _ac) / (_c * _ab) * _eyc.Pi();
-    }
-    Math::real y_c(Math::real sbet, Math::real cbet) const {
-      using std::sqrt;
-      Math::real sbet1 = _ab * sbet, cbet1 = _ac * cbet;
-      norm(sbet1, cbet1);
-      Math::real dbet1 = _eyc.Delta(sbet1, cbet1);
-      return (_ac/_b) *
-        ( Math::sq(_b) / (_c * _ab) * _eyc.Pi(sbet1, cbet1, dbet1) -
-          (0 ?
-           Math::asinh(_bc2 * sbet * cbet / sqrt(Math::sq(_b * sbet) * _ab2 +
-                                                 Math::sq(_c * cbet) * _ac2 ))
-           :
-           Math::atanh(_bc2 * sbet1 * cbet1 /
-                       sqrt(Math::sq(_b * sbet1) * _ac2 +
-                            Math::sq(_c * cbet1) * _ab2))) );
-    }
-    Math::real y_c(Math::real bet) const {
-      using std::abs; using std::sin; using std::cos;
-      Math::real
-        a = bet * Math::degree(),
-        sbet = abs(bet) == 180 ? 0 : sin(a),
-        cbet = abs(bet) ==  90 ? 0 : cos(a);
-      return y_c(sbet, cbet);
-    }
-    Math::real x_bx() const {
-      return EllipticFunction(Math::sq(_c/_b)*_ab2 / _ac2, _ab2 / _ac2).G();
-    }
-    Math::real y_bx() const {
-      return EllipticFunction(Math::sq(_a/_b)*_bc2 / _ac2, _bc2 / _ac2).G();
-      // tan(nu) = _bc/_ab; sin(nu) = _bc/_ac; cos(nu) = _ab/_ac
-      // cot(nu) = _ab/_bc; csc(nu) = _ac/_bc; sec(nu) = _ac/_ab
-    }
-    Math::real x_cx() const {
-      return Math::sq(_a/_b) *
-        EllipticFunction(Math::sq(_c/_b)*_ab2 / _ac2,
-                         -_ab2 / Math::sq(_b)).Pi();
-    }
-    Math::real y_cx() const {
-      return Math::sq(_c/_b) *
-        EllipticFunction(Math::sq(_a/_b)*_bc2 / _ac2,
-                         _bc2 / Math::sq(_b)).Pi();
+      return y(sbet, cbet) / Math::degree();
     }
   };
 
