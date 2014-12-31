359b527b3ff57c399be9d7800111c3b7f8274132
Charles Karney <karney@users.sourceforge.net>
2014-12-31 09:04:58 -0500

    Jacobi: simplify notation.

--- examples/JacobiConformal.hpp
+++ examples/JacobiConformal.hpp
@@ -24,10 +24,11 @@ namespace GeographicLib {
    * a sphere or an ellipsoid of revolution.
    **********************************************************************/
   class JacobiConformal {
-    Math::real _a, _b, _c, _ab2, _bc2, _ac2;
+    typedef Math::real real;
+    real _a, _b, _c, _ab2, _bc2, _ac2;
     EllipticFunction _ex, _ey;
-    static void norm(Math::real& x, Math::real& y)
-    { Math::real z = Math::hypot(x, y); x /= z; y /= z; }
+    static void norm(real& x, real& y)
+    { real z = Math::hypot(x, y); x /= z; y /= z; }
   public:
     /**
      * Constructor for a trixial ellipsoid with semi-axes
@@ -40,7 +41,7 @@ namespace GeographicLib {
      * \e c.  This form of the constructor cannot be used to specify a
      * sphere (use the next constructor).
      **********************************************************************/
-    JacobiConformal(Math::real a, Math::real b, Math::real c)
+    JacobiConformal(real a, real b, real c)
       : _a(a), _b(b), _c(c)
       , _ab2((_a - _b) * (_a + _b))
       , _bc2((_b - _c) * (_b + _c))
@@ -68,8 +69,7 @@ namespace GeographicLib {
      * (<i>a</i>&minus;<i>b</i>) : (<i>b</i>&minus;<i>c</i>) with \e ab
      * &ge; 0, \e bc &be; 0, and \e ab + \e bc > 0.
      **********************************************************************/
-    JacobiConformal(Math::real a, Math::real b, Math::real c,
-                    Math::real ab, Math::real bc)
+    JacobiConformal(real a, real b, real c, real ab, real bc)
       : _a(a), _b(b), _c(c)
       , _ab2(ab * (_a + _b))
       , _bc2(bc * (_b + _c))
@@ -97,8 +97,8 @@ namespace GeographicLib {
      * @param[in] comg cos(&omega;)
      * @return \e x
      **********************************************************************/
-    Math::real x(Math::real somg, Math::real comg) const {
-      Math::real somg1 = _b * somg, comg1 = _a * comg; norm(somg1, comg1);
+    Math::real x(real somg, real comg) const {
+      real somg1 = _b * somg, comg1 = _a * comg; norm(somg1, comg1);
       return Math::sq(_a / _b)
         * _ex.Pi(somg1, comg1, _ex.Delta(somg1, comg1));
     }
@@ -110,9 +110,9 @@ namespace GeographicLib {
      *
      * &omega; must be in (&minus;180&deg;, 180&deg;].
      **********************************************************************/
-    Math::real x(Math::real omg) const {
+    Math::real x(real omg) const {
       using std::abs; using std::sin; using std::cos;
-      Math::real
+      real
         a = omg * Math::degree(),
         somg = abs(omg) == 180 ? 0 : sin(a),
         comg = abs(omg) ==  90 ? 0 : cos(a);
@@ -129,8 +129,8 @@ namespace GeographicLib {
      * @param[in] cbet cos(&beta;)
      * @return \e y
      **********************************************************************/
-    Math::real y(Math::real sbet, Math::real cbet) const {
-      Math::real sbet1 = _b * sbet, cbet1 = _c * cbet; norm(sbet1, cbet1);
+    Math::real y(real sbet, real cbet) const {
+      real sbet1 = _b * sbet, cbet1 = _c * cbet; norm(sbet1, cbet1);
       return Math::sq(_c / _b)
         * _ey.Pi(sbet1, cbet1, _ey.Delta(sbet1, cbet1));
     }
@@ -142,9 +142,9 @@ namespace GeographicLib {
      *
      * &beta; must be in (&minus;180&deg;, 180&deg;].
      **********************************************************************/
-    Math::real y(Math::real bet) const {
+    Math::real y(real bet) const {
       using std::abs; using std::sin; using std::cos;
-      Math::real
+      real
         a = bet * Math::degree(),
         sbet = abs(bet) == 180 ? 0 : sin(a),
         cbet = abs(bet) ==  90 ? 0 : cos(a);
