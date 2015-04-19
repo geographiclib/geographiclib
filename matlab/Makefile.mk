1782df652207b50c3862a10c6828e75d1dd85965
Charles Karney <karney@users.sourceforge.net>
2015-04-19 10:35:23 -0400

    Fix build bugs

--- matlab/Makefile.mk
+++ matlab/Makefile.mk
@@ -1,16 +1,4 @@
-MATLAB_FILES = geoddoc.m geodreckon.m geoddistance.m geodarea.m \
-	defaultellipsoid.m ecc2flat.m flat2ecc.m \
-	geodproj.m eqdazim_fwd.m eqdazim_inv.m cassini_fwd.m cassini_inv.m \
-	tranmerc_fwd.m tranmerc_inv.m gnomonic_fwd.m gnomonic_inv.m \
-	gedoc.m gereckon.m gedistance.m
-
-MATLAB_LEGACY = utmupsforward_a.m utmupsreverse_a.m mgrsforward_a.m \
-	mgrsreverse_a.m geodesicdirect_a.m geodesicinverse_a.m \
-	geodesicline_a.m geoidheight_a.m geocentricforward_a.m \
-	geocentricreverse_a.m localcartesianforward_a.m \
-	localcartesianreverse_a.m polygonarea_a.m
-
-MATLAB_FILES = $(wildcard geographiclib/private/*.m)
+MATLAB_FILES = $(wildcard geographiclib/*.m)
 MATLAB_PRIVATE = $(wildcard geographiclib/private/*.m)
 MATLAB_LEGACY = $(wildcard geographiclib_legacy/*.m)
 
