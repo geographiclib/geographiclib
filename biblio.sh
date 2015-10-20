77f2f4141102d786b45507b2e183f9c7f78e4070
Charles Karney <karney@users.sourceforge.net>
2015-10-20 10:09:21 -0400

    Various html fixes.  Use zenodo doi's where available.

--- biblio.sh
+++ biblio.sh
@@ -9,8 +9,7 @@ cat <<'EOF'
       Online geodesic bibliography
     </title>
     <link rel="stylesheet" type="text/css" href="../default.css">
-    <meta http-equiv="Content-Type"
-	  content="text/html; charset=ISO-8859-1">
+    <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
   </head>
   <body topmargin=10 leftmargin=10>
     <h3>An online geodesic bibliography</h3>
