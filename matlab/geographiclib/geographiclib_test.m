41efef44fbef253e374e3b0ceb4e05c935ad3689
Charles Karney <karney@users.sourceforge.net>
2017-12-18 17:11:16 -0500

    Workaround bugs in fmod+sin in VS-201[012] in C implementation. Relax delta for distance check in GeodSolve59; 2.5e-9 nm is too strict since 1 ulp = 3.7e-9 nm; use new delta = 5e-9 nm.

--- matlab/geographiclib/geographiclib_test.m
+++ matlab/geographiclib/geographiclib_test.m
@@ -457,7 +457,7 @@ function n = GeodSolve59
   [s12, azi1, azi2] = geoddistance(5, 0.00000000000001, 10, 180);
   n = n + assertEquals(azi1, 0.000000000000035, 1.5e-14);
   n = n + assertEquals(azi2, 179.99999999999996, 1.5e-14);
-  n = n + assertEquals(s12, 18345191.174332713, 2.5e-9);
+  n = n + assertEquals(s12, 18345191.174332713, 5e-9);
 end
 
 function n = GeodSolve61
